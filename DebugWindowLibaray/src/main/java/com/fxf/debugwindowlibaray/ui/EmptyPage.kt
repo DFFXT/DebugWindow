c68ba78aeb42ea6007878c8d509c3cb122cb9616
qinf <qinf@meixing.com>
2023-09-12 10:20:20 +0800

    tint 应该外部自己设置

--- DebugWindowLibaray/src/main/java/com/fxf/debugwindowlibaray/ui/EmptyPage.kt
+++ DebugWindowLibaray/src/main/java/com/fxf/debugwindowlibaray/ui/EmptyPage.kt
@@ -1,8 +1,11 @@
 package com.fxf.debugwindowlibaray.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.fxf.debugwindowlibaray.R
 
 /**
@@ -13,6 +16,12 @@ class EmptyPage : UIPage() {
     override fun enableTouch(): Boolean = false
 
     override fun enableFocus(): Boolean = false
+    override fun onCreateTabView(ctx: Context, parent: ViewGroup): View {
+        return super.onCreateTabView(ctx, parent).apply {
+            this as ImageView
+            imageTintList = ColorStateList.valueOf(Color.WHITE)
+        }
+    }
 
     override fun getTabIcon(): Int = R.mipmap.view_debug_common_close
 
